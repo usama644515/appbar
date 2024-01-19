766c7e39808dc5f64aef63e0e1d7bebc73edd316
Usama <hanifusama688@gmail.com>
2024-01-19 19:57:49 +0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+export default function App() {
+  // const [age, setAge] = useState(18);
+  const [fvalue, setfvalue] = useState();
+  const [svalue, setsvalue] = useState();
+  const [op, setop] = useState();
+  const [result, setresult] = useState();
+  let handleclick = () => {
+    if (op == "+") {
+      setresult(fvalue + svalue);
+    } else if (op == "-") {
+      setresult(fvalue - svalue);
+    } else if (op == '*') {
+      setresult(fvalue * svalue);
+    } else {
+      setresult(fvalue / svalue);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={(e) => { setfvalue(parseInt(e.target.value)) }} />
+      <select onChange={(e) => { setop(e.target.value) }}>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input onChange={(e) => { setsvalue(parseInt(e.target.value)) }} />
+      <button onClick={handleclick}>Calculate</button>
+      <br />
+      <h2>The Result is {result}</h2>
+      {/* {age < 18 ? <h2>You are child</h2> : age == 18 ? <h2>Your age is 18</h2> : <h2>You are adult</h2>} */}
     </div>
   );
 }
 
-export default App;
+
+
---
+++ src/NewScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+export default function NewScreen(props) {
+  return (
+    <div>
+      <h2>{props.val1}</h2>
+    </div>
+  );
+}
